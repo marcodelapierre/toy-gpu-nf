6ccc171a261f5e06a169262c367754a6b85667f8
Marco De La Pierre <marco.delapierre@gmail.com>
2021-04-28 17:10:46 +0800

    using hacked multi-cluster nextflow

--- job.sh
+++ job.sh
@@ -3,14 +3,13 @@
 #SBATCH --job-name=toy-gpu
 #SBATCH --account=pawsey0001
 #SBATCH --clusters=zeus
-#SBATCH --partition=debugq
+#SBATCH --partition=workq
 #SBATCH --time=10:00
 #SBATCH --export=none
 
 unset SBATCH_EXPORT
+module use /group/director2172/software/sles12sp3/modulefiles #not needed for users whose default project is director2172
 module load singularity
-module load nextflow
+module load nextflow/20.07.1-multi
 
-export SLURM_CLUSTERS="topaz"
-
-nextflow run main.nf -profile pawsey
+nextflow run main.nf -profile zeus
